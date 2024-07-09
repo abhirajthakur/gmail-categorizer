6fe0548c4298cca97a1472cd50ab7044099b468e
Abhiraj Thakur <abhirajthakur124@gmail.com>
2024-07-09 12:46:40 +0530

    move classify emails to lib

--- components/EmailList.tsx
+++ components/EmailList.tsx
@@ -1,133 +1,133 @@
 "use client";
 
-import { classifyEmails } from "@/actions/classify";
 import { fetchEmails } from "@/actions/emails";
 import Email from "@/components/Email";
 import EmailLoadingSkeleton from "@/components/EmailLoadingSkeleton";
 import SelectedEmail from "@/components/SelectedEmail";
 import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
+import { classifyEmails } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function EmailList({ accessToken }: { accessToken: string }) {
-  const [emailMessages, setEmailMessages] = useState<Email[] | undefined>([]);
-  const [emailsCount, setEmailsCount] = useState<string>("15");
-  const [selectedEmail, setSelectedEmail] = useState<Email | null>(null);
-  const [emailCategories, setEmailCategories] = useState<{
-    [key: string]: string;
-  }>();
+	const [emailMessages, setEmailMessages] = useState<Email[] | undefined>([]);
+	const [emailsCount, setEmailsCount] = useState<string>("15");
+	const [selectedEmail, setSelectedEmail] = useState<Email | null>(null);
+	const [emailCategories, setEmailCategories] = useState<{
+		[key: string]: string;
+	}>();
 
-  const { mutate: server_getEmails, isPending: isFetchingEmails } = useMutation(
-    {
-      mutationFn: fetchEmails,
-      onSuccess: (data) => {
-        setEmailMessages(data);
-      },
-    },
-  );
+	const { mutate: server_getEmails, isPending: isFetchingEmails } = useMutation(
+		{
+			mutationFn: fetchEmails,
+			onSuccess: (data) => {
+				setEmailMessages(data);
+			},
+		},
+	);
 
-  const { mutate: server_classifyEmails, isPending: isClassifyingEmails } =
-    useMutation({
-      mutationFn: classifyEmails,
-      onSuccess: (data) => {
-        setEmailCategories(data);
-      },
-    });
+	const { mutate: server_classifyEmails, isPending: isClassifyingEmails } =
+		useMutation({
+			mutationFn: classifyEmails,
+			onSuccess: (data) => {
+				setEmailCategories(data);
+			},
+		});
 
-  const handleClassifyEmails = () => {
-    server_classifyEmails({
-      emails: emailMessages!,
-      apiKey: localStorage.getItem("api_key")!,
-    });
-  };
+	const handleClassifyEmails = () => {
+		server_classifyEmails({
+			emails: emailMessages!,
+			apiKey: localStorage.getItem("api_key")!,
+		});
+	};
 
-  const handleEmailCountChange = (e: string) => {
-    setEmailMessages([]);
-    setEmailCategories({});
-    setEmailsCount(e);
-  };
+	const handleEmailCountChange = (e: string) => {
+		setEmailMessages([]);
+		setEmailCategories({});
+		setEmailsCount(e);
+	};
 
-  const handleEmailSelect = (email: Email) => {
-    setSelectedEmail(email);
-  };
+	const handleEmailSelect = (email: Email) => {
+		setSelectedEmail(email);
+	};
 
-  useEffect(() => {
-    server_getEmails({
-      accessToken: accessToken,
-      maxResults: Number(emailsCount),
-    });
-  }, [emailsCount]);
+	useEffect(() => {
+		server_getEmails({
+			accessToken: accessToken,
+			maxResults: Number(emailsCount),
+		});
+	}, [emailsCount]);
 
-  if (isFetchingEmails || isClassifyingEmails) {
-    return (
-      <div className="space-y-4 p-4">
-        <EmailLoadingSkeleton />
-        <EmailLoadingSkeleton />
-        <EmailLoadingSkeleton />
-        <EmailLoadingSkeleton />
-        <EmailLoadingSkeleton />
-      </div>
-    );
-  }
+	if (isFetchingEmails || isClassifyingEmails) {
+		return (
+			<div className="space-y-4 p-4">
+				<EmailLoadingSkeleton />
+				<EmailLoadingSkeleton />
+				<EmailLoadingSkeleton />
+				<EmailLoadingSkeleton />
+				<EmailLoadingSkeleton />
+			</div>
+		);
+	}
 
-  return (
-    <div className="no-scrollbar flex h-screen w-full overflow-y-scroll">
-      <div className="flex-1">
-        <div className="fixed top-[4.5rem] flex w-full items-center justify-between border border-b-gray-300 bg-white p-4 px-7">
-          <div>
-            <Select
-              value={emailsCount.toString()}
-              onValueChange={handleEmailCountChange}
-            >
-              <SelectTrigger className="bg-white text-gray-900">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="15">15</SelectItem>
-                <SelectItem value="20">20</SelectItem>
-                <SelectItem value="50">50</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+	return (
+		<div className="no-scrollbar flex h-screen w-full overflow-y-scroll">
+			<div className="flex-1">
+				<div className="fixed top-[4.5rem] flex w-full items-center justify-between border border-b-gray-300 bg-white p-4 px-7">
+					<div>
+						<Select
+							value={emailsCount.toString()}
+							onValueChange={handleEmailCountChange}
+						>
+							<SelectTrigger className="bg-white text-gray-900">
+								<SelectValue />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value="15">15</SelectItem>
+								<SelectItem value="20">20</SelectItem>
+								<SelectItem value="50">50</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
 
-          <div>
-            <Button
-              variant="outline"
-              className="gap-1 rounded-full"
-              onClick={handleClassifyEmails}
-            >
-              <Image src="/icons/tags.svg" alt="tags" width={20} height={20} />
-              <span className="text-xs">Classify</span>
-            </Button>
-          </div>
-        </div>
+					<div>
+						<Button
+							variant="outline"
+							className="gap-1 rounded-full"
+							onClick={handleClassifyEmails}
+						>
+							<Image src="/icons/tags.svg" alt="tags" width={20} height={20} />
+							<span className="text-xs">Classify</span>
+						</Button>
+					</div>
+				</div>
 
-        <div className="space-y-4 p-4 pt-20">
-          {emailMessages?.map((email) => (
-            <Email
-              key={email.id}
-              email={email}
-              category={emailCategories?.[email.id]}
-              handleEmailSelect={handleEmailSelect}
-            />
-          ))}
-        </div>
-      </div>
+				<div className="space-y-4 p-4 pt-20">
+					{emailMessages?.map((email) => (
+						<Email
+							key={email.id}
+							email={email}
+							category={emailCategories?.[email.id]}
+							handleEmailSelect={handleEmailSelect}
+						/>
+					))}
+				</div>
+			</div>
 
-      {selectedEmail && (
-        <SelectedEmail
-          selectedEmail={selectedEmail}
-          setSelectedEmail={setSelectedEmail}
-        />
-      )}
-    </div>
-  );
+			{selectedEmail && (
+				<SelectedEmail
+					selectedEmail={selectedEmail}
+					setSelectedEmail={setSelectedEmail}
+				/>
+			)}
+		</div>
+	);
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,100 +5,100 @@ import { twMerge } from "tailwind-merge";
 import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 export const categories = [
-  { name: "important", color: "#4CAF50" },
-  { name: "promotions", color: "#2196F3" },
-  { name: "social", color: "#FF9800" },
-  { name: "marketing", color: "#673AB7" },
-  { name: "spam", color: "#9E9E9E" },
-  { name: "general", color: "#FF2450" },
+	{ name: "important", color: "#4CAF50" },
+	{ name: "promotions", color: "#2196F3" },
+	{ name: "social", color: "#FF9800" },
+	{ name: "marketing", color: "#673AB7" },
+	{ name: "spam", color: "#9E9E9E" },
+	{ name: "general", color: "#FF2450" },
 ];
 
 export function getInitials(fullName: string) {
-  const names = fullName.split(" ");
-  let initials = names[0].substring(0, 1).toUpperCase();
+	const names = fullName.split(" ");
+	let initials = names[0].substring(0, 1).toUpperCase();
 
-  if (names.length > 1) {
-    initials += names[names.length - 1].substring(0, 1).toUpperCase();
-  }
-  return initials;
+	if (names.length > 1) {
+		initials += names[names.length - 1].substring(0, 1).toUpperCase();
+	}
+	return initials;
 }
 
 export const classifyEmails = async ({
-  emails,
-  apiKey,
+	emails,
+	apiKey,
 }: {
-  emails: Email[];
-  apiKey: string;
+	emails: Email[];
+	apiKey: string;
 }) => {
-  const categories: { [key: string]: string } = {};
+	const categories: { [key: string]: string } = {};
 
-  for (const email of emails ?? []) {
-    const emailCategory = await getEmailCategory({
-      email: email.content,
-      apiKey: apiKey,
-    });
-    categories[email.id] = emailCategory;
-  }
+	for (const email of emails ?? []) {
+		const emailCategory = await getEmailCategory({
+			email: email.content,
+			apiKey: apiKey,
+		});
+		categories[email.id] = emailCategory;
+	}
 
-  console.log(categories);
+	console.log(categories);
 
-  return categories;
+	return categories;
 };
 
 const getEmailCategory = async ({
-  email,
-  apiKey,
+	email,
+	apiKey,
 }: {
-  email: string;
-  apiKey: string;
+	email: string;
+	apiKey: string;
 }): Promise<string> => {
-  // const categories = [
-  //   "important",
-  //   "promotions",
-  //   "social",
-  //   "marketing",
-  //   "spam",
-  //   "general",
-  // ];
-  //
-  // const randomIndex = Math.floor(Math.random() * categories.length);
-  // return categories[randomIndex];
+	// const categories = [
+	//   "important",
+	//   "promotions",
+	//   "social",
+	//   "marketing",
+	//   "spam",
+	//   "general",
+	// ];
+	//
+	// const randomIndex = Math.floor(Math.random() * categories.length);
+	// return categories[randomIndex];
 
-  const taggingPrompt = ChatPromptTemplate.fromTemplate(
-    "Extract the desired information from the following passage.\nOnly extract the properties mentioned in the 'Classification' function.\nPassage:\n{input}",
-  );
+	const taggingPrompt = ChatPromptTemplate.fromTemplate(
+		"Extract the desired information from the following passage.\nOnly extract the properties mentioned in the 'Classification' function.\nPassage:\n{input}",
+	);
 
-  const classificationSchema = z.object({
-    category: z.enum([
-      "important",
-      "promotions",
-      "social",
-      "marketing",
-      "spam",
-      "general",
-    ]),
-  });
+	const classificationSchema = z.object({
+		category: z.enum([
+			"important",
+			"promotions",
+			"social",
+			"marketing",
+			"spam",
+			"general",
+		]),
+	});
 
-  const llm = new ChatOpenAI({
-    apiKey,
-    temperature: 0,
-    model: "gpt-3.5-turbo",
-  });
+	const llm = new ChatOpenAI({
+		apiKey,
+		temperature: 0,
+		model: "gpt-3.5-turbo",
+	});
 
-  // Name is optional, but gives the models more clues as to what your schema represents
-  const llmWihStructuredOutput = llm.withStructuredOutput(
-    classificationSchema,
-    {
-      name: "category extractor",
-    },
-  );
+	// Name is optional, but gives the models more clues as to what your schema represents
+	const llmWihStructuredOutput = llm.withStructuredOutput(
+		classificationSchema,
+		{
+			name: "category extractor",
+		},
+	);
 
-  const taggingChain = taggingPrompt.pipe(llmWihStructuredOutput);
+	const taggingChain = taggingPrompt.pipe(llmWihStructuredOutput);
 
-  const res = await taggingChain.invoke({ input: email });
-  return res.category;
+	const res = await taggingChain.invoke({ input: email });
+	return res.category;
 };
